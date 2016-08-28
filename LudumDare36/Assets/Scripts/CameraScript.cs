0cdb1e99b66169e24a0481f98c924b2e86afce20
Erickson <cortezericksonb@gmail.com>
2016-08-28 13:19:38 +0800

    * Added camera following boulder

---
+++ LudumDare36/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    Transform myTransform;
+    Vector3 offset;
+
+    void Start()
+    {
+        myTransform = transform;
+        offset = myTransform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        myTransform.DOMove(desiredPosition, Time.deltaTime);
+    }
+}
