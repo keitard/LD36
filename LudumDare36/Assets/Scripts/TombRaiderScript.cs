46ea882fbcaed9c4e8783babb0bbe1a13c3d6e8b
Erickson <cortezericksonb@gmail.com>
2016-08-28 19:17:42 +0800

    *Added sample tomb raiders * Added object pooling

--- LudumDare36/Assets/Scripts/TombRaiderScript.cs
+++ LudumDare36/Assets/Scripts/TombRaiderScript.cs
@@ -6,18 +6,40 @@ public class TombRaiderScript : MonoBehaviour
 {
     public Transform target;
     Transform myTransform;
+    Rigidbody myRigidbody;
+
 
     public float speed = 2f;
 
-    void Start()
+    void Awake()
     {
         myTransform = transform;
-        myTransform.DOMove(new Vector3(target.position.x, transform.position.y, target.position.z), speed).SetEase(Ease.Linear);
+        myRigidbody = GetComponent<Rigidbody>();
+        target = GameObject.Find("GoldenMonkeyTreasure").transform;
     }
 
-    void Update()
+    void OnEnable()
     {
+        myRigidbody.DOMove(new Vector3(target.position.x, myTransform.position.y, target.position.z), speed).SetEase(Ease.Linear).SetAutoKill(false).SetSpeedBased();
+
+        myTransform.LookAt(target.position);
+    }
+    
+    void OnDisable()
+    {
+        myRigidbody.DOKill();
+        myTransform.position = Vector3.zero;
+        myTransform.eulerAngles = Vector3.zero;
+        print("Die!");
 
     }
 
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+            gameObject.SetActive(false);
+        }
+    }
 }
---
+++ LudumDare36/Assets/Scripts/TombRaiderSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TombRaiderSpawner : MonoBehaviour
+{
+    public ObjectPooler objectPooler;
+    public Transform[] spawnerTransform;
+
+    public float timing = 0;
+
+    void Update()
+    {
+        timing += Time.deltaTime;
+        if(timing >= 3f)
+        {
+            GameObject tombRaider = objectPooler.GetPooledObject();
+            tombRaider.transform.position = spawnerTransform[Random.Range(0, spawnerTransform.Length)].position;
+            tombRaider.SetActive(true);
+            timing = 0;
+        }
+    }
+
+}
