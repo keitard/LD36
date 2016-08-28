fac4a6f13542981182a8fcabf74c9b45d0d04911
Erickson <cortezericksonb@gmail.com>
2016-08-28 13:04:13 +0800

    * Added 3D objects * Added boulder * Added script for boulder behaviour

---
+++ LudumDare36/Assets/Scripts/BoulderScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderScript : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+
+    public float forceRate = 5f;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        myRigidbody.AddForce(new Vector3(horizontal, 0, vertical) * forceRate, ForceMode.Force);
+    }
+
+}
--- LudumDare36/Assets/Scripts/LookAtCenter.cs
+++ LudumDare36/Assets/Scripts/LookAtCenter.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class LookAtCenter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        transform.LookAt(new Vector3(0, 0.75f, 0));
+    }
 }
